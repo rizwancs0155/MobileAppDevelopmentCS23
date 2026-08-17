f80e86fb2e1147defb02d07ee30852ca5f3f1c6c
Rizwan.cs0155 <Rizwan.cs0155@gmail.com>
2026-08-17 21:25:23 +0500

    Task on Activity State

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,51 @@
 package com.example.myapplication;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        imageView = findViewById(R.id.imageView);
+        radioGroup = findViewById(R.id.radioGroup);
+
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+
+                    @Override
+                    public void onCheckedChanged(
+                            RadioGroup group,
+                            int checkedId) {
+
+                        if (checkedId == R.id.radioApple) {
+
+                            imageView.setImageResource(
+                                    R.drawable.apple
+                            );
+
+                        } else if (checkedId == R.id.radioBanana) {
+
+                            imageView.setImageResource(
+                                    R.drawable.banana
+                            );
+
+                        } else if (checkedId == R.id.radioOrange) {
+
+                            imageView.setImageResource(
+                                    R.drawable.orange
+                            );
+                        }
+                    }
+                });
     }
 }
